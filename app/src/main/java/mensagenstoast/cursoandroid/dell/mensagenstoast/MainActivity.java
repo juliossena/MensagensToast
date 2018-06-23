e740c6e7ba37b24f9c6ab9d799c016c02be78da6
juliossena <juliosouzasena@gmail.com>
2018-06-23 17:19:17 -0300

    final

--- app/src/main/java/mensagenstoast/cursoandroid/dell/mensagenstoast/MainActivity.java
+++ app/src/main/java/mensagenstoast/cursoandroid/dell/mensagenstoast/MainActivity.java
@@ -2,12 +2,26 @@ package mensagenstoast.cursoandroid.dell.mensagenstoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botao = (Button) findViewById(R.id.botao_id);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Mensagem Teste", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
